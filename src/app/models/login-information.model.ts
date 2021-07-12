a53286bced80f30132272180e7960314392d683a
Medusy <fatma.keskin65@erzurum.edu.tr>
2021-07-12 17:11:32 +0300

    loginInformation constructor

--- src/app/models/login-information.model.ts
+++ src/app/models/login-information.model.ts
@@ -1,2 +1,8 @@
 export class LoginInformation {
+    constructor(email:string , password:string){
+        this.email = email;
+        this.password = password;
+    }
+    public email: string;
+    public password : string;
 }
