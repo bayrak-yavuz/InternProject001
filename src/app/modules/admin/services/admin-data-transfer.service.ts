b62c98ac381fd27b78ffd0cc599b94039c3d4a53
HasanHuseyinGumustas <hasangumustas@gmail.com>
2021-07-12 18:10:15 +0300

    admin-data-transfer.service (Logout fonksiyonu)

--- src/app/modules/admin/services/admin-data-transfer.service.ts
+++ src/app/modules/admin/services/admin-data-transfer.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminDataTransferService {
 
-  constructor() { }
+  constructor(
+
+    private angFireLogout: AngularFireAuth
+  ) { }
 
   logout(){
+    this.angFireLogout.signOut();
     // TODO: Çıkış yapmak için gerekli fonksiyon
   }
 
