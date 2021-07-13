c5512d45f5814d9d6097d2230d668b3ebb20fc1d
HasanHuseyinGumustas <hasangumustas@gmail.com>
2021-07-13 17:10:09 +0300

    appID değişikliği. appID olarak çağırılacak.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment = {
     storageBucket: 'internproject001.appspot.com',
     messagingSenderId: '946828851723',
     appId: '1:946828851723:web:2db268b19fe11deaaf250e',
+    appID: 'InternProject001',
     measurementId: 'G-FB8H5VSX9K'
   }
 };
