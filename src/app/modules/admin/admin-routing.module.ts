bf37cadc6a43b4fbe5262d924c9e24ad5d9446a8
mehmetkoroglu <mkoroglu334@gmail.com>
2021-07-13 16:52:33 +0300

    new routes added

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardPage } from './pages/dashboard/dashboard.page';
 import { RootPage } from './pages/root/root.page';
+import { PostsPage } from './pages/posts/posts.page';
+import { CatogoriesPage } from './pages/catogories/catogories.page';
 
 const routes: Routes = [
   {path: '', component: RootPage, children: [
     {path: '', component: DashboardPage},
+    {path: '', component: PostsPage},
+    {path: '', component: CatogoriesPage}
   ]}
 ];
 
