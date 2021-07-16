5574e393f41a7da6bf3301265013f04da9930d9d
mehmetkoroglu <mkoroglu334@gmail.com>
2021-07-16 16:55:54 +0300

    categories component updated

--- src/app/modules/admin/pages/categories/categories.page.ts
+++ src/app/modules/admin/pages/categories/categories.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'admin-categories',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.page.scss']
 })
 export class CategoriesPage implements OnInit {
-
-  constructor() { }
+  array: any[] = [];  
+  constructor(private cs: CategoriesService) { }
 
   ngOnInit(): void {
+    this.getCats();
+  }
+
+  getCats() {
+    this.cs.getCategories().then(res => {
+      res.subscribe(s => {
+        s.docs.forEach(doc => { this.array.push({data: doc.data(), id: doc.id})})
+        this.array = this.array.sort((a: any, b: any) => (a?.categoryName + b?.categoryName))
+      })
+    });
   }
 
 }
