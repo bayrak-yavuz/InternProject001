3d49fd90998999acbc16784476c78fcca369cd33
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-07-16 11:15:48 +0300

    post and categories routing

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -4,12 +4,16 @@ import { DashboardPage } from './pages/dashboard/dashboard.page';
 import { RootPage } from './pages/root/root.page';
 import { PostsPage } from './pages/posts/posts.page';
 import { CategoriesPage } from './pages/categories/categories.page';
+import { PostEditPage } from './pages/post-edit/post-edit.page';
+import { CategoriesEditPage } from './pages/categories-edit/categories-edit.page';
 
 const routes: Routes = [
   {path: '', component: RootPage, children: [
     {path: '', component: DashboardPage},
     {path: 'yazilar', component: PostsPage},
-    {path: 'kategoriler', component: CategoriesPage}
+    {path: 'kategoriler', component: CategoriesPage},
+    {path: 'yazi-duzenle', component: PostEditPage},
+    {path: 'kategori-duzenle', component: CategoriesEditPage}
   ]}
 ];
 
